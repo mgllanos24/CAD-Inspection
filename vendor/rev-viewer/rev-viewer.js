078ed9854ff646f6e87c7afe55e0fff85ee61258
mgllanos24 <mgllanos@yahoo.com>
2025-09-08 21:25:41 -0700

    Add basic rev-viewer implementation with importedImage

--- vendor/rev-viewer/rev-viewer.js
+++ vendor/rev-viewer/rev-viewer.js
@@ -1,1 +1,31 @@
-// Placeholder for rev-viewer.js
+import { importedImage } from './tools.js';
+
+export class Viewer {
+  constructor(container) {
+    this.container = container;
+    this.imageElement = null;
+  }
+
+  async load(file) {
+    const objectUrl = URL.createObjectURL(file);
+    try {
+      const img = await importedImage(objectUrl);
+      img.style.maxWidth = '100%';
+      img.style.maxHeight = '100%';
+      this.container.appendChild(img);
+      this.imageElement = img;
+    } catch (err) {
+      const msg = document.createElement('div');
+      msg.textContent = `Unable to display ${file.name}.`;
+      this.container.appendChild(msg);
+    }
+  }
+
+  dispose() {
+    if (this.imageElement) {
+      this.container.removeChild(this.imageElement);
+      URL.revokeObjectURL(this.imageElement.src);
+      this.imageElement = null;
+    }
+  }
+}
---
+++ vendor/rev-viewer/tools.js
@@ -0,0 +1,8 @@
+export function importedImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error('Failed to load image'));
+    img.src = url;
+  });
+}
