27fad340bb4f7c56c251c44512c068ec4fe9633b
mgllanos24 <mgllanos@yahoo.com>
2025-09-14 13:57:18 -0700

    refactor: serve assets from static folder

--- server.py
+++ server.py
@@ -1,22 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/vendor/<path:filename>')
-def vendor(filename):
-    return send_from_directory('vendor', filename)
-
-@app.route('/main.js')
-def main_js():
-    return send_from_directory('.', 'main.js')
-
-@app.route('/style.css')
-def style_css():
-    return send_from_directory('.', 'style.css')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
